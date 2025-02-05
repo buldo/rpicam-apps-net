8e554185022ddb350967cfdc436a7e846b6bfc1f
Roman Buldygin <froooks@gmail.com>
2025-02-06 00:58:36 +0300

    Add project files.

---
+++ JpegCapture/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace JpegCapture
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
